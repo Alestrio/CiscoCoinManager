5ce4185fd4cf2c1a505e525eb8126d56a99b6112
Alexis LEBEL <lebalexis@gmail.com>
2020-11-08 17:31:01 +0100

    UserChangePasswordView.kt

---
+++ src/main/kotlin/org/alestrio/kcoinmanager/view/user/UserChangePasswordView.kt
@@ -0,0 +1,15 @@
+package org.alestrio.kcoinmanager.view.user
+
+import com.github.mvysny.karibudsl.v10.div
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.router.Route
+import org.alestrio.kcoinmanager.Application
+
+@Route("user-changepassword",layout = Application::class)
+class UserChangePasswordView: VerticalLayout() {
+    init {
+        div {
+
+        }
+    }
+}
