a77e6a18fc3baaeead5aed16121a6eaf46a99e49
Alexis LEBEL <lebalexis@gmail.com>
2020-11-12 17:00:23 +0100

    User dashboard

--- src/main/kotlin/org/alestrio/kcoinmanager/data/model/Transaction.kt
+++ src/main/kotlin/org/alestrio/kcoinmanager/data/model/Transaction.kt
@@ -1,18 +1,17 @@
 package org.alestrio.kcoinmanager.data.model
 
-import com.github.vokorm.KEntity
+import com.github.vokorm.*
 import com.gitlab.mvysny.jdbiorm.Dao
 import java.time.Instant
-import javax.validation.constraints.NotEmpty
 
 data class Transaction(
     /**
      * This is the database model storing a transaction
      */
     override var id : Long? = null,
-    private var source:String = "",
-    private var destination:String = "",
-    private var amount:Int = 0
+    var source:String = "",
+    var destination:String = "",
+    var amount:Int = 0
 
 ) : KEntity<Long> {
     override fun save(validate: Boolean){
--- src/main/kotlin/org/alestrio/kcoinmanager/view/user/UserDashboard.kt
+++ src/main/kotlin/org/alestrio/kcoinmanager/view/user/UserDashboard.kt
@@ -1,19 +1,29 @@
 package org.alestrio.kcoinmanager.view.user
 
-import com.github.mvysny.karibudsl.v10.div
+import com.github.mvysny.karibudsl.v10.*
+import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.data.provider.DataProvider
 import com.vaadin.flow.router.Route
 import eu.vaadinonkotlin.vaadin10.Session
 import org.alestrio.kcoinmanager.Application
 import org.alestrio.kcoinmanager.LoginService
+import org.alestrio.kcoinmanager.data.model.Transaction
+import org.alestrio.kcoinmanager.data.model.User
 
 @Route("user-dashboard",layout = Application::class)
 class UserDashboard: VerticalLayout() {
     private val loginService: LoginService = getLoginService()
+    private val currentUser: User? = loginService.currentUser
     init {
-        if(loginService.currentUser != null) {
+        if(currentUser != null) {
             div {
-
+                h1("Tableau de bord :")
+                verticalLayout {
+                    h2("Bienvenue ${currentUser.pseudo} !")
+                    h2("Votre solde de CiscoCoins est de : " + currentUser.balance)
+                    h2("Voici un résumé de vos transactions :")
+                }.add(getGridOfTransactions())
             }
         }
         else{
@@ -28,5 +38,25 @@ class UserDashboard: VerticalLayout() {
             Session["loginService"] = service
         }
         return service
+    }
+
+    private fun getGridOfTransactions(user:User = currentUser!!):Grid<Transaction>{
+        fun getOnlyUserInvolvedTransactions(): MutableList<Transaction> {
+            val allTrx = Transaction.findAll()
+            val invTrx: MutableList<Transaction> = ArrayList()
+            allTrx.forEach {
+                if(it.source == user.pseudo || it.destination == user.pseudo){
+                    invTrx.add(it)
+                }
+            }
+            return allTrx
+        }
+        return grid(dataProvider = DataProvider.ofCollection(getOnlyUserInvolvedTransactions())){
+            flexGrow = 1.0
+            addColumnFor(Transaction::id)
+            addColumnFor(Transaction::source)
+            addColumnFor(Transaction::amount)
+            addColumnFor(Transaction::destination)
+        }
     }
 }
