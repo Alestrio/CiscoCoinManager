d2f10f10e1a0d07e1f9347fc63b738c13a82e51c
Alexis LEBEL <lebalexis@gmail.com>
2020-11-13 16:54:12 +0000

    [Repl.it] Transaction grid for admin

--- src/main/kotlin/org/alestrio/kcoinmanager/view/admin/TransactionsView.kt
+++ src/main/kotlin/org/alestrio/kcoinmanager/view/admin/TransactionsView.kt
@@ -1,11 +1,12 @@
 package org.alestrio.kcoinmanager.view.admin
 
-import com.github.mvysny.karibudsl.v10.div
+import com.github.mvysny.karibudsl.v10.*
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.Route
 import eu.vaadinonkotlin.vaadin10.Session
 import org.alestrio.kcoinmanager.Application
 import org.alestrio.kcoinmanager.LoginService
+import org.alestrio.kcoinmanager.Transaction
 
 @Route("transactions",layout = Application::class)
 class TransactionsView: VerticalLayout() {
@@ -13,7 +14,14 @@ class TransactionsView: VerticalLayout() {
     init {
         if(loginService.currentUser != null && loginService.currentUser!!.isAdmin) {
             div {
-
+                h1("Tableau des transactions :")
+                grid(dataProvider = DataProvider.ofCollection(Transaction.findAll())){
+                  flexGrow = 1.0
+                  addColumnFor(Transaction::id)
+                  addColumnFor(Transaction::source)
+                  addColumnFor(Transaction::amount)
+                  addColumnFor(Transaction::destination)
+                }
             }
         }
         else{
